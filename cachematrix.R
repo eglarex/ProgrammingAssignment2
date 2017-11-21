07da13fbf42064676230714c773ca8dc9502ea47
eglarex <33787403+eglarex@users.noreply.github.com>
2017-11-21 18:06:24 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+#############################################################
+## Matrix inversion is usually a costly computation and 
+## there may be some benefit to caching the inverse of 
+## a matrix rather than compute it repeatedly
+##
+## Two functions are consturcted to complete the inversion:
+##
+## 1. makeCacheMatrix: This function creates a special 
+## "matrix" object that can cache its inverse.
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+  im<-NULL
+  set<-function(y){
+    x<<-y
+    im<<-NULL
+  }
+  get<-function()x
+  setinver<-function(inver)im<<-inver
+  getinver<-function()im
+  list(set=set,get=get,setinver=setinver,getinver=getinver)
 }
 
-
-## Write a short comment describing this function
-
+############################################################
+## 2. cacheSolve: This function computes the inverse of the 
+## special "matrix" returned by makeCacheMatrix above. If 
+## the inverse has already been calculated (and the matrix 
+## has not changed), then the cachesolve should retrieve 
+## the inverse from the cache.
+##
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        im<-x$getinver()
+        if(!is.null(im)){
+          message("getting cached data")
+          return(im)
+        }
+        data<-x$get()
+        im<-solve(data)
+        x$setinver(im)
+        im
 }
